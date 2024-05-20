bcc23d928a18c09b0d5ca01b42e0a25222363ca4
Dimitrios Papaioannou <dimitrispoloco@gmail.com>
2024-05-20 15:30:25 +0200

    whitespace

--- ufocus/main.py
+++ ufocus/main.py
@@ -12,11 +12,14 @@ from main_window import main
 
 logging.config.dictConfig(LOGGING_CONFIG)
 
+
 def run_main() -> int:
     exit_code: int = main()
     return exit_code
 
+
 if __name__ == "__main__":
     import sys
+
     exit_code = main()
     sys.exit(exit_code)

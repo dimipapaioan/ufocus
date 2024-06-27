02bb8c53a4968b22b96e41324beda0081f60b011
Dimitrios Papaioannou <dimitrispoloco@gmail.com>
2024-06-27 12:58:40 +0200

    Update to newest pyside6

--- ufocus/main_window.py
+++ ufocus/main_window.py
@@ -100,7 +100,7 @@ CUSTOM_STYLESHEET = """
 
 ABOUT = """
 <p><b><font size='+1'>The μFocus Application</font></b></p>
-<p>Version: 2.2.1</p>
+<p>Version: 2.2.2</p>
 <p>Author: Dimitrios Papaioannou
 <a href = "mailto: dimipapaioan@outlook.com"> dimipapaioan@outlook.com </a> </p>
 """

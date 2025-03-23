80aca1a1282ee91010e9d992c7a2039617a9ccd8
Dimitrios Papaioannou <dimitrispoloco@gmail.com>
2025-03-23 12:07:59 +0100

    Improve minimizer logs with statistics

--- ufocus/minimizer.py
+++ ufocus/minimizer.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import logging
-from math import pi, sqrt, nan
+from dataclasses import dataclass
+from math import nan, inf, isnan
 
 from PySide6.QtCore import (
     QObject,
@@ -12,6 +13,7 @@ from PySide6.QtCore import (
 from scipy.optimize import minimize, OptimizeResult
 
 from image_processing import DetectedEllipse
+from ps_controller import PSController
 from settings_manager import SettingsManager
 
 
@@ -27,9 +29,32 @@ class MinimizerSignals(QObject):
     finished = Signal()
 
 
-class Minimizer(QRunnable):
+@dataclass
+class PSCurrentsInfo:
+    ps1_previous: float = nan
+    ps2_previous: float = nan
+    ps1_min: float = nan
+    ps2_min: float = nan
 
-    def __init__(self, pscontroller, mutex, condition, parent=None):
+    def update(self, c_new: list[float]) -> None:
+        self.ps1_previous = float(c_new[0]) 
+        self.ps2_previous = float(c_new[1])
+
+    def update_min(self, min_new: list[float]) -> None:
+        self.ps1_min = float(min_new[0])
+        self.ps2_min = float(min_new[1])
+
+
+@dataclass
+class ObjectiveFunctionInfo:
+    previous: float = nan
+    delta: float = nan
+    min_val: float = nan
+    min_delta: float = nan
+
+
+class Minimizer(QRunnable):
+    def __init__(self, pscontroller: PSController, mutex, condition, parent=None) -> None:
         super().__init__(parent)
         self.parent = parent
         self.pscontroller = pscontroller
@@ -41,10 +66,12 @@ class Minimizer(QRunnable):
         self.settings_manager = SettingsManager()
         self.settings_manager.saveUserSettings()
         self.signals = MinimizerSignals()
+        self.obj_func_stats = ObjectiveFunctionInfo()
+        self.ps_currents_stats = PSCurrentsInfo()
 
         logger.info("Minimizer initialized")
 
-    def run(self):
+    def run(self) -> None:
         logger.info("Minimizer started")
         self.pscontroller.signals.controlTimer.emit(True)
         self.signals.inAccumulation.emit(False)
@@ -60,7 +87,7 @@ class Minimizer(QRunnable):
         ]
 
         try:
-            self.solution = minimize(
+            self.solution: OptimizeResult = minimize(
                 fun=self.function,
                 x0=initial,
                 method="Nelder-Mead",
@@ -81,12 +108,11 @@ class Minimizer(QRunnable):
                 fun=nan, nit=0, nfev=0, status=99, x=[nan, nan]
             )
         except ValueError:
-            logger.error("Incorrect bounds")
+            logger.error(f"Incorrect bounds: {bounds}")
             self.signals.boundsError.emit()
         else:
             logger.info(f"Solution: {self.solution.x}")
             self.signals.setCurrent.emit(self.solution.x)
-            # print(self.solution.allvecs)
         finally:
             self.pscontroller.refreshGUI()
             self.pscontroller.updateDialValue(self.pscontroller.ps1, self.parent.psLCD1)
@@ -95,21 +121,24 @@ class Minimizer(QRunnable):
             self.pscontroller.signals.controlTimer.emit(False)
             self.signals.finished.emit()
 
-    def callback(self, intermediate_result):
+    def callback(self, intermediate_result: OptimizeResult) -> None:
         logger.info(
             f"=== ITERATION ENDED ===\n"
             f"Best solution so far:\n"
             f"Q1 = {intermediate_result.x[0]:.4f} A\n"
             f"Q2/3 = {intermediate_result.x[1]:.4f} A\n"
-            f"Min. Func. = {intermediate_result.fun:.4f}\n"
+            f"Obj. Func. = {intermediate_result.fun:.4f}\n"
         )
         if self.control:
             raise StopIteration
 
-    def function(self, x):
+    def function(self, x) -> float:
+        # Update current statistics
+        self.ps_currents_stats.update(x)
+
         if not self.control:
             logger.info(
-                f"Min. Func. called with parameters: {x[0]:.4f} A, {x[1]:.4f} A"
+                f"Obj. Func. called with parameters: {x[0]:.4f} A, {x[1]:.4f} A"
             )
             # These values are to be sent to the power supplies
             self.signals.updateCurrent.emit(x)
@@ -136,33 +165,53 @@ class Minimizer(QRunnable):
             # uses to decide the next step. At this point maybe the value could be sent
             # to someplace else inside the code. The same functionality could be achieved
             # with the callback function.
-            # res = (a * b) ** 2 * (
-                # 1 / (4 * pi**2 * a * b / self.calculate_perimeter(a, b) ** 2) ** 3
-            # ) - 1.0
-            res = ellipse.area / ellipse.circularity ** 2
+            res = ellipse.area / ellipse.circularity**2
+
             self.signals.updateFunction.emit(res)
+
+            self.update_statistics(x, res)
+
             self.pscontroller.refreshGUI()
-            logger.info(f"Min. Func. return value: {res:.2f}")
+
+            logger.info(f"Obj. Func. return value: {res:.2f}")
+            logger.info(f"Objective funcion statistics: {self.obj_func_stats}")
+            logger.info(f"PS currents statistics: {self.ps_currents_stats}")
         else:
             raise StopIteration
         return res
 
-    @Slot(list)
-    def get_res(self, ellipse_data: DetectedEllipse):
+    @Slot(DetectedEllipse)
+    def get_res(self, ellipse_data: DetectedEllipse) -> None:
         logger.debug("Got values from image processing")
         self.res = ellipse_data
         self.condition.wakeAll()
 
-    def set_res(self):
+    def set_res(self) -> DetectedEllipse:
         logger.debug("Set values to minimizer")
         return self.res
 
-    def calculate_perimeter(self, a, b):
-        return pi * (3 * (a + b) - sqrt((3 * a + b) * (a + 3 * b)))
-    
     @Slot(list)
-    def setPSCurrents(self, x):
-        logger.info(f'Setting Q1 current to: {x[0]}')
+    def setPSCurrents(self, x: list[float]) -> None:
+        logger.info(f"Setting Q1 current to: {x[0]}")
         self.pscontroller.setPS1Current(x[0] * 100)
-        logger.info(f'Setting Q2/3 currents to: {x[1]}')
+        logger.info(f"Setting Q2/3 currents to: {x[1]}")
         self.pscontroller.setPS2Current(x[1] * 100)
+    
+    def update_statistics(self, currents: list[float], obj_ret: float) -> None:
+        if not isnan(self.obj_func_stats.min_val):
+            if self.obj_func_stats.min_val > obj_ret:
+                self.obj_func_stats.min_val = obj_ret
+                self.ps_currents_stats.update_min(currents)
+                
+            delta = obj_ret - self.obj_func_stats.previous
+            if delta < self.obj_func_stats.min_delta:
+                self.obj_func_stats.min_delta = delta
+                
+            self.obj_func_stats.delta = obj_ret - self.obj_func_stats.previous
+            self.obj_func_stats.previous = obj_ret
+                
+        else:
+            self.obj_func_stats.min_val = obj_ret
+            self.obj_func_stats.previous = obj_ret
+            self.obj_func_stats.min_delta = inf
+            self.ps_currents_stats.update_min(currents)

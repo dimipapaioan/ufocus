dcf684196519ef8c5800d7ceaaa2509aed88b638
Dimitrios Papaioannou <dimitrispoloco@gmail.com>
2025-03-23 18:07:06 +0100

    Bump version to 2.3.0

--- ufocus/main_window.py
+++ ufocus/main_window.py
@@ -101,7 +101,7 @@ CUSTOM_STYLESHEET = """
 
 ABOUT = """
 <p><b><font size='+1'>The μFocus Application</font></b></p>
-<p>Version: 2.2.4</p>
+<p>Version: 2.3.0</p>
 <p>Author: Dimitrios Papaioannou
 <a href = "mailto: dimipapaioan@outlook.com"> dimipapaioan@outlook.com </a> </p>
 """
